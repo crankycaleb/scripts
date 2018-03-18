4bd764f0eff21d0a514786dcdeede33c688b6f1a
crankycaleb <caleb@crankycaleb.io>
2018-03-18 18:03:57 -0500

    Update nextcloud_selinux.sh

--- nextcloud_selinux.sh
+++ nextcloud_selinux.sh
@@ -3,7 +3,7 @@ ocpath='/var/www/html/nextcloud'
 httpdrw='httpd_sys_rw_content_t'
 setsebool -P httpd_can_sendmail 1
 setsebool -P httpd_can_network_connect 1
-printf "Set SELinux HTTPD RW on  Directories\n"
+printf "Set SELinux HTTPD RW on directories\n"
 semanage fcontext -a -t ${httpdrw} "${ocpath}/config(/.*)?"
 restorecon -R ${ocpath}/config
 semanage fcontext -a -t ${httpdrw} "${ocpath}/apps(/.*)?"
